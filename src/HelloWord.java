7025c36d0aa6cab4f25b779141a40ea0ae7fd339
bambi <310686540@qq.com>
2020-05-30 23:34:11 +0800

    提交了

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,7 +2,7 @@ public class HelloWord {
     public static void main(String[] args){
         int i=0;
         System.out.println("这是生产的一个bug");
-        int j=1/0;
+        System.out.println("c修改了bug");
 
         System.out.println("初始化项目");
     }
