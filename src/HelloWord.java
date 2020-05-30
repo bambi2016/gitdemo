69beec1d871b4889b4cde65be04cc72ee2a7e1f1
bambi <310686540@qq.com>
2020-05-30 22:13:00 +0800

    生产版本

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,5 +1,9 @@
 public class HelloWord {
     public static void main(String[] args){
+        int i=0;
+        System.out.println("这是生产的一个bug");
+        int j=1/0;
+
         System.out.println("初始化项目");
     }
 }
