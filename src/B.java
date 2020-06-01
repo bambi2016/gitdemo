8e22d305e978e213b292758e4405c5b94441f06f
bambi <310686540@qq.com>
2020-06-02 00:10:27 +0800

    enen

---
+++ src/B.java
@@ -0,0 +1,11 @@
+public class B {
+    public void fun1(){
+        System.out.println("B创建了一个方法");
+    }
+    public void fun2(){
+        System.out.println("B创建了第二个方法");
+    }
+    public void fun3(){
+        System.out.println("muster创建 origin 了一个方法");
+    }
+}
